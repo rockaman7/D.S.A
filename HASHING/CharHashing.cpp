3f00003ada5d3818c5ea0d6930b69239db8bd44c
rockaman7 <kr.amank27@gmail.com>
2024-09-19 19:09:09 +0530

    Update CharHashing.cpp

--- HASHING/CharHashing.cpp
+++ HASHING/CharHashing.cpp
@@ -6,19 +6,25 @@ int main (){
     cout<< "input string: "; 
     cin >>s ;
 
-    //prefetch
-    int hash [26]={0};
-    for (int i = 0 ; i<=s.size();i++){
-        hash [s[i]-'a']++;  // [char - 'a'] will give index of the character at which we have to do hashing
-                            // s[i] is the position of characters in the input string 
-    } 
+    //  pre computation i.e. storing count in hash array 
 
+    int hash [256]={0}; //when string has both upper and smaller case characters
+    for (int i = 0 ; i<=s.size();i++){
+        hash [s[i]]++;   // s[i] is the position of characters in the input string               
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------
+    int hash [26]={0};                  //when string has only either upper case or smaller case characters
+    for (int i = 0 ; i<=s.size();i++){
+        hash [s[i]-'a']++;               // [char - 'a'] will give index of the character at which we have to do hashing || s[i] is the position of characters in the input string 
+    }
+    -------------------------------------------------------------------------------------------------------------------*/
     cout<< "no of query:  "<< endl ;
     int q ;
     cin >> q ;
     while ( q--){
         char ch ;
-        cout << "input queries: "<< endl ;
+        cout << "input query: "<< endl ;
         cin >> ch ;
         cout << "frequency: ";
         cout << hash[ch-'a']<<endl;
