a44432951f763e602f6692517f753b383b145256
rockaman7 <kr.amank27@gmail.com>
2024-09-26 18:16:41 +0530

    count frequency of elements of array
    using 1.two loops and 2. Map

--- HASHING/countFreq1.cpp
+++ HASHING/countFreq1.cpp
@@ -1,21 +1,19 @@
 //Count frequency of each element in the array
-// using two loop
+
 
 #include<bits/stdc++.h>
 using namespace std ;
-
+// using two loop
 void countFreq(int arr[],int n){
 
     vector <bool> visited(n,false);
     //visited vector with n elements.
     //All n elements in the visited vector will be initialized to false.
     for ( int i=0; i<n; i++){
-
-        if (visited[i] == true )
+        if (visited[i] == true)
         continue;
 
-        //count freq
-        int count=1;
+        int count=1;//count freq
         for (int j=i+1; j<n; j++){ 
         // taking j= i+1 as code compares arr[0] with the remaining elements (arr[1], arr[2], ..., arr[n-1]) to find duplicates.
         //If we started with j = i (i.e., j = 0), it would compare arr[0] with itself, which is pointless because arr[0]
@@ -27,6 +25,18 @@ void countFreq(int arr[],int n){
         cout << "Frequency of "<<arr[i]<<"->"<<count<< endl; 
     }                           
 }
+//Using Map
+/*void Frequency(int arr[], int n)
+{
+    unordered_map<int, int> map;
+ 
+    for (int i = 0; i < n; i++)
+        map[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto x : map)
+        cout << x.first << " " << x.second << endl;
+}*/
 
 int main (){
     int n ;
