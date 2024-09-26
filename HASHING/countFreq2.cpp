93c58c262142f35fd321e8c2924fa9bfafca0a13
rockaman7 <kr.amank27@gmail.com>
2024-09-27 01:42:18 +0530

    max/min frequency elements in array
    using
    1.two loops
    2. Map

--- HASHING/countFreq2.cpp
+++ HASHING/countFreq2.cpp
@@ -1,9 +1,9 @@
-//find the highest/lowest frequency element in Array of size n 
+// find the highest/lowest frequency element in Array of size n 
 
 #include <bits/stdc++.h>
 using namespace std ;
-//using two loops
-
+//-----------------------------using two loops-------------------------
+/*
 void findFreq(int arr[],int n){
     vector <bool> checked(n,false);
     int maxFreq=0 ;
@@ -33,7 +33,33 @@ void findFreq(int arr[],int n){
     }
     cout<<"Max Frequency Element: "<< maxElement<< " : Frequency: "<<maxFreq<<endl ;
     cout<<"Min Frequency Element: "<< minElement<< " : Frequency: "<<minFreq<<endl ;
+} */
+//----------------------using MAP----------------------------------------------------
+void findFreq(int arr[],int n){
+    unordered_map<int,int> m;
+    int maxFreq=0 , minFreq = n;
+    int maxElement= 0 , minElement = 0;
+    for(int i=0;i<n;i++){ // traverse the array and count frequency 
+        m[arr[i]]++;
+    }
+    //to iterate through the map
+    for (auto it:m){
+        int element= it.first;
+        int count= it.second;   
+
+        if(count>maxFreq){
+            maxElement= element;
+            maxFreq = count;
+        }
+        if ( count < minFreq){
+            minElement= element;
+            minFreq = count;
+        }
+    }
+    cout<<"Max Frequency Element: "<< maxElement<< " and Frequency: "<<maxFreq<<endl ;
+    cout<<"Min Frequency Element: "<< minElement<< " and Frequency: "<<minFreq<<endl ;
 }
+//------------------------------------------------------------------------------------
 int main(){
     int n ;
     cout<<"enter array size: "<<endl;
