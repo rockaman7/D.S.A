5ab9284be5b7f5baa52c80f465bba70f3d1d3ee5
rockaman7 <kr.amank27@gmail.com>
2024-10-06 00:10:24 +0530

    Fibonacci number at position
    Fibonacci number at corresponding position in Fibonacci Series

--- RECURSION/FibonacciPosition.cpp
+++ RECURSION/FibonacciPosition.cpp
@@ -1,5 +1,7 @@
-// Fibonacci  no using recursion (we use MULTIPLE recursion concept)
-//e.g. - 1 1 2 3 5 8 13 21
+// Write the Fibonacci number by its position in Fibonacci series using recursion (we use MULTIPLE recursion concept)
+
+//e.g. -  1 1 2 3 5 8 13 21
+//at position 6 , number is 8
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,8 +18,9 @@ int fib(int n){
 
 int main ( ){
     int n;
-    cout<< "n: ";
+    cout<< "Enter any position in Fibonacci Series : ";
     cin>> n ;
+    cout<< "Corresponding Fibonacci number: ";
     cout << fib(n); // we are finding fibonacci number at nth position of fibonacci series
     return 0;
 }
